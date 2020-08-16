8edd016cb0f893005adb79a6082a85a23145f1cc
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-08-17 02:05:21 +1000

    Add files via upload

--- FelixNetCoreAppDemo/Models/Employee.cs
+++ FelixNetCoreAppDemo/Models/Employee.cs
@@ -7,5 +7,18 @@ namespace FelixNetCoreAppDemo.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public int DepartmentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public bool Fired { get; set; }
+    }
+
+    // Enum for Gender, female is 0, male is 1
+    public enum Gender
+    {
+        female = 0,
+        male = 1
     }
 }
