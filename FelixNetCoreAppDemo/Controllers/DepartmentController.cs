7b1baad4e2c29698f083558239e8f73b967a8a56
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-08-17 02:07:14 +1000

    Add files via upload

---
+++ FelixNetCoreAppDemo/Controllers/DepartmentController.cs
@@ -0,0 +1,25 @@
+﻿using FelixNetCoreAppDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FelixNetCoreAppDemo.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentService _departmentService;
+        public DepartmentController(IDepartmentService departmentService)   
+        {
+            _departmentService = departmentService;
+        }
+
+        public async Task<IActionResult> Index() 
+        {
+            ViewBag.title = "Department Index";
+            var departments = _departmentService.GetAll();
+            return View(departments);
+        }
+    }
+}
