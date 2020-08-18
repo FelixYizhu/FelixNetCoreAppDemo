29d9d3d0b7d57edf774a97e8c39d53a767b4e78c
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-08-19 02:53:02 +1000

    Add files via upload

--- FelixNetCoreAppDemo/ViewComponents/CompanySummaryViewComponent.cs
+++ FelixNetCoreAppDemo/ViewComponents/CompanySummaryViewComponent.cs
@@ -1,1 +1,25 @@
+﻿using FelixNetCoreAppDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace FelixNetCoreAppDemo.ViewComponents
+{
+    public class CompanySummaryViewComponent: ViewComponent
+    {
+        private readonly IDepartmentService _departmentService;
+        public CompanySummaryViewComponent(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string title)
+        {
+            ViewBag.Title = title;
+            var summary = await _departmentService.GetCompanySummary();
+            return View(summary);
+        }
+    }
+}
