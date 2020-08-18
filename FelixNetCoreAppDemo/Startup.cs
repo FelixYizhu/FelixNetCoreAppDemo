8c771bff4a570dfd23eeb224b95c6828fe5a3ff8
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-08-19 02:07:18 +1000

    Add files via upload

--- FelixNetCoreAppDemo/Startup.cs
+++ FelixNetCoreAppDemo/Startup.cs
@@ -6,6 +6,7 @@ using FelixNetCoreAppDemo.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,6 +14,14 @@ namespace FelixNetCoreAppDemo
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,6 +37,8 @@ namespace FelixNetCoreAppDemo
 
             services.AddSingleton<IEmployeeService, EmployeeService>();
 
+            // map the config item
+            services.Configure<ConfigOptions>(_configuration.GetSection("SetBold"));
 
 
             // Add support for Razor pages
@@ -57,7 +68,7 @@ namespace FelixNetCoreAppDemo
                 // Map controller route
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}"  //default no input name of controller, it is Home
+                    pattern: "{controller=Department}/{action=Index}/{id?}"  //default no input name of controller, it is Home
                 );
             });
         }
