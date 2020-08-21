b9e9eec507ad418405158908cd45ffc00035ea71
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-08-21 23:27:51 +1000

    Add files via upload

--- FelixNetCoreAppDemo/Controllers/EmployeeController.cs
+++ FelixNetCoreAppDemo/Controllers/EmployeeController.cs
@@ -19,6 +19,42 @@ namespace FelixNetCoreAppDemo.Controllers
             _employeeService = employeeService;
         }
 
+        public async Task <IActionResult> Index(int departmentId) 
+        {
+            var department = await _departmentService.GetById(departmentId);
+            ViewBag.Title = $"Employees of {department.Name}";
+            ViewBag.DepartmentId = departmentId;
+
+            var employees = await _employeeService.GetByDepartmentId(departmentId);
+            return View(employees);
+        }
+
+        public IActionResult Add(int departmentId)
+        {
+            ViewBag.Title = "Add Employee";
+            //return a model with departmentId to the View page
+            return View(new Employee
+            {
+                DepartmentId = departmentId
+            }) ;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _employeeService.Add(model);
+            }
+            return RedirectToAction(nameof(Index),routeValues: new { departmentId=model.DepartmentId});  //Index page needs a parameter of departmentId, so pass it
+         }
+
+        public async Task<IActionResult> Fire(int employeeId)
+        {
+            var employee = await _employeeService.Fire(employeeId);
+            return RedirectToAction(nameof(Index),routeValues: new { departmentId=employee.DepartmentId});
+        }
+
 
     }
 }
